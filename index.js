c6add0df93cb47f8cf053feaea2a3ba8a3d9d430
KevinLiu05 <kaiwenliu0529@gmail.com>
2025-03-13 18:01:38 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 module.exports = (req, res) => {
     res.status(200).json({
       message: "Welcome to UW Room Booking API",
       availableRoutes: ["/api/search", "/api/advanced-search"],
       documentation: "访问 /api/search 和 /api/advanced-search 使用 POST 请求"
     });
+=======
+module.exports = (req, res) => {
+    res.status(200).json({
+      message: "Welcome to UW Room Booking API",
+      availableRoutes: ["/api/search", "/api/advanced-search"],
+      documentation: "访问 /api/search 和 /api/advanced-search 使用 POST 请求"
+    });
+>>>>>>> bae8c230bc388cad2c369e1764d9724f7fce87d7
   };
